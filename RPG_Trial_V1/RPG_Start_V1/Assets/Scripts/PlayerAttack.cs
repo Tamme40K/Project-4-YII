e664ef42f3d3126edeaf2cb44d2716238f298cfe
Lou-Anne <0950196@hr.nl>
2019-05-29 13:44:29 +0200

    Lou-Anne Test V0.1
    Beweging, animation, button to attack (Not finished) en begin van HP/MP/attk/Defence...

---
+++ RPG_Trial_V1/RPG_Start_V1/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerAttack : MonoBehaviour
+{
+
+    public Button bTN;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Button").GetComponent<Button>();
+        bTN.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void TaskOnClick()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button!");
+    }
+}
