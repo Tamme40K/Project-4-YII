494f9265b26acb3509d0abf37f9dc35dba3c5da1
Tamme <0951595@hr.nl>
2019-06-12 11:48:01 +0200

    Nieuwe tutorial map, camera, room transition

--- Battle Tendency RPG/Assets/Scripts/RoomMove.cs
+++ Battle Tendency RPG/Assets/Scripts/RoomMove.cs
@@ -5,36 +5,15 @@ using UnityEngine.UI;
 
 public class RoomMove : MonoBehaviour
 {
-    public Vector2 cameraChange; //hoeveel moet camera veranderen bij trigger
-    public Vector3 playerChange; //hoeveel moet player veranderen bij trigger
-    private CameraMovement cam; //cameramovement script
     public bool needText; //heeft de room waar ik in ga text nodig? true or false
     public string placeName; //welke tekst moet verschijnen
     public GameObject text; //de gameobject voor text in Unity zelf
     public Text placeText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        cam = Camera.main.GetComponent<CameraMovement>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
-            //Je pakt de cam en gebruikt de min/max van cameramovement op de min/maxposition aan te passen (de boundaries)..
-            //..als je de trigger raakt
-            cam.minPosition += cameraChange;
-            cam.maxPosition += cameraChange;
-            other.transform.position += playerChange; //je verplaats je player ook na de trigger anders sta je op 'oude' map
-
             //hebben we tekst nodig? zo ja, activate textobject en text = placeName
             if (needText)
             {
