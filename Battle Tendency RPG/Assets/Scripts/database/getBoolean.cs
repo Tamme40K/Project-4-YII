98f38f689d937e7516c6bba41ace26ddeea85106
Gregory Kaman <0934829@hr.nl>
2019-07-05 12:56:53 +0200

    v0.11
    created booleanDatabase
    created functions to change booleanValue

--- Battle Tendency RPG/Assets/Scripts/database/getBoolean.cs
+++ Battle Tendency RPG/Assets/Scripts/database/getBoolean.cs
@@ -8,13 +8,20 @@ public class getBoolean : MonoBehaviour
     public Text name;
     private string testphrase;
     private string playerName;
+    private string fireBooleanValue;
+    private string holyBooleanValue;
+    private bool fireBooleanStatus;
+    private bool holyBooleanStatus;
 
     // Use this for initialization
     void Start()
     {
         var getBoolean = new DataService("booleanholder.db");
         string nickname2 = (PlayerPrefs.GetString("tutorialTextKeyName"));
+        //getBoolean.ChangeFireBoolean("false", nickname2);
+        //getBoolean.ChangeHolyBoolean("true", nickname2);
         //getBoolean.CreateDB ();
+        //getBoolean.CreateDB();
         //var people = ds.GetPersons ();
         //ToConsole (people);
 
@@ -23,13 +30,40 @@ public class getBoolean : MonoBehaviour
         //ToConsole("New person has been created");
         //getBoolean.CreateBooleanHolder(nickname2);
 
-        //var nickname = getNickname.GetNickname(nickname2);
-        //Debug.Log(nickname);
-        //testphrase = nickname.ToString();
-        //Debug.Log(testphrase);
-        //string[] testzin = testphrase.Split(',');
-        //Debug.Log(testzin[1]);
-        //playerName = testzin[1];
+        var boolval = getBoolean.GetHolyBooleanValue(nickname2);
+        Debug.Log(boolval);
+        testphrase = boolval.ToString();
+        Debug.Log(testphrase);
+        string[] testzin = testphrase.Split(',');
+        Debug.Log(testzin[3]);
+        Debug.Log(testzin[4]);
+        fireBooleanValue = testzin[3];
+        holyBooleanValue = testzin[4];
+
+        if (fireBooleanValue == "true")
+        {
+            fireBooleanStatus = true;
+        }
+        else
+        {
+            if (fireBooleanValue == "false")
+            {
+                fireBooleanStatus = false;
+            }
+        }
+
+        if (holyBooleanValue == "true")
+        {
+            holyBooleanStatus = true;
+        }
+        else
+        {
+            if (holyBooleanValue == "false")
+            {
+                holyBooleanStatus = false;
+            }
+        }
+
         //Debug.Log(playerName);
         //name.text = playerName;
 
