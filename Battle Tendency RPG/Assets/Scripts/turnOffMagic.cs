03737bda4c5174d12484da810712bf9c3ba71976
Gregory Kaman <0934829@hr.nl>
2019-07-06 22:10:15 +0200

    added turnOffMagicWall + finalHero death brengt je naar victoryScene

---
+++ Battle Tendency RPG/Assets/Scripts/turnOffMagic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class turnOffMagic : ItemManager
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            fireItem = false;
+            holyItem = false;
+            ChangeFireValue(false);
+            ChangeHolyValue(false);
+            Debug.Log(fireItem);
+            Debug.Log(holyItem);
+        }
+    }
+}
