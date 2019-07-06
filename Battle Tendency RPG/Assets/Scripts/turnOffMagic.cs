3fdd775a1257a56e62f6ff231dedb2caff598b73
Gregory Kaman <0934829@hr.nl>
2019-07-06 20:01:42 +0200

    player loses magic after tutorial
    player loses magic powers after tutorial. KAN NOG WEL GEBRUIKEN TOT ER SCENE WORD GERELOAD!!

---
+++ Battle Tendency RPG/Assets/Scripts/turnOffMagic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnOffMagic : ItemManager
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            fireItem = false;
+            holyItem = false;
+            ChangeFireValue(false);
+            ChangeHolyValue(false);
+            Debug.Log(fireItem);
+            Debug.Log(holyItem);
+        }
+    }
+
+
+}
